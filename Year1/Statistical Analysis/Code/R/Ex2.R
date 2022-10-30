3b9c0ed7905bdcc161c8916149d4226d08bc06ad
FOAM-1 <84202132+FOAM-1@users.noreply.github.com>
2022-10-30 18:12:34 +0000

    Ex2 solutions

---
+++ Year1/Statistical Analysis/Code/R/Ex2.R
@@ -0,0 +1,30 @@
+# load in all the files we need
+library(glue) # lets us use f-string like comprehension
+
+
+# path where the dataset is stored
+path <- "C:/Users/James/Documents/GitHub/birckbeck_applied_statistics/Year1/Statistical Analysis/Data/sugar.txt"
+
+# load in the data 
+sugar <- read.csv(path, dec="\t", sep="\t")
+
+print(head(sugar))
+
+# get the column names as a list
+cols <- colnames(sugar)
+
+# lop through all the columns and convert the dtypes to numerics
+for (col in cols){
+  sugar[, c(col)] <- as.numeric(sugar[, c(col)])
+}
+
+# make a new column which is the natural log of consumption
+sugar["lconsump"] = log(sugar$consump)
+
+# loop through a list of columns, starting from column 2
+for (col in colnames(sugar)[(2:3)]){
+  plot(sugar[, c("price"), sugar[, c(col)]], xlab="price", ylab=col, main=glue("Plot of {col} vs Price"))
+}
+
+
+
---
+++ Year1/Statistical Analysis/Exercises/Exercise Solutions/Exercise 2.md
@@ -0,0 +1,43 @@
+## Question a
+
+$\large{consump = A e^{\alpha \hspace{2mm} price}}$
+
+$\large{ln(consump) = ln(A e ^{\alpha \hspace{2mm} price})}$
+
+$= ln(A) + \alpha \hspace{2mm} price$
+
+We now have a linear model in them form 
+
+$\large{y = \beta_0 + \beta_1 X}$
+
+where 
+- $y = ln(consump)$
+- $\beta_0 = ln(A)$
+- $\beta_1 = \alpha$
+- $X = price$
+
+-------------
+## Question B
+
+![[Pasted image 20221030173402.png]]
+
+![[Pasted image 20221030173409.png]]
+
+The fit is more linear in the log case
+
+--------
+## Question c
+
+![[Pasted image 20221030173815.png]]
+
+---------
+## Question d
+
+When prices are 6 cents per pound, we get that consumption is 70 pounds. 
+
+----------
+## Question e
+
+![[Pasted image 20221030181115.png]]
+
+
